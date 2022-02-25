011bcd58c7b462b141ae2a4d6fa4ad05fbf25040
sam bacha <sam@manifoldfinance.com>
2022-02-25 01:48:42 -0800

    fix(lint): format

--- WalletConnectExperience.tsx
+++ WalletConnectExperience.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { Text, TouchableOpacity, StyleSheet } from "react-native";
-import { useWalletConnect } from "@walletconnect/react-native-dapp";
-import { convertUtf8ToHex } from "@walletconnect/utils";
+import * as React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useWalletConnect } from '@walletconnect/react-native-dapp';
+import { convertUtf8ToHex } from '@walletconnect/utils';
 
 function Button({ onPress, label }: any) {
   return (
@@ -20,7 +20,7 @@ export default function WalletConnectExperience() {
 
   const signRequest = () => {
     connector.signPersonalMessage([
-      convertUtf8ToHex("Hello world!"),
+      convertUtf8ToHex('Hello world!'),
       connector.accounts[0],
     ]);
   };
@@ -46,15 +46,15 @@ export default function WalletConnectExperience() {
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "#5A45FF",
-    color: "#FFFFFF",
+    backgroundColor: '#5A45FF',
+    color: '#FFFFFF',
     borderRadius: 12,
     paddingHorizontal: 16,
     paddingVertical: 12,
   },
   text: {
-    color: "#FFFFFF",
+    color: '#FFFFFF',
     fontSize: 16,
-    fontWeight: "600",
+    fontWeight: '600',
   },
 });
